2e16e6b07915e24649d49d336499829f118f6595
Chris Scott <cyberstrike@gmail.com>
2015-02-14 22:03:02 -0500

    wip unable to register bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  class NotAuthorized < StandardError; end
 
-  validates_presence_of :email
   has_many :articles
 end
